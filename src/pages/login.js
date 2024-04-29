a2bc30e2d5a5a5568fcee5dafb67613f7091d2af
David Zamarrón <a01284723@tec.mx>
2024-04-29 17:28:09 -0600

    cambios

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function login() {
+    return (
+        <div className='flex justify-content flex-end'><h1>Login</h1>
+        </div>
+    )
+}
+
+export default login
---
+++ src/pages/videogame.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function videogame() {
+  return (
+    <div>videogame</div>
+  )
+}
+
+export default videogame
