b928337e589043ac6040af2d648b204185d0d443
David Zamarrón <a01284723@tec.mx>
2024-04-29 18:17:44 -0600

    cambios

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 function login() {
     return (
-        <div className='flex justify-content flex-end'><h1>Login</h1>
+        <div  className='flex flex-col items-center h-screen'>
+            <h1 className='font-bold mb-20'>Esta es la pagina de Login</h1>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Rockwell_Automation_logo_%282019%29.svg/2560px-Rockwell_Automation_logo_%282019%29.svg.png'
+            alt='logo'
+            className='h-20 mb-10 '></img>
+            <form className='flex flex-col space-y-4 h-30 mb-8'>
+                <input type='email' placeholder='E-Mail' className='p-2 border border-gray-300 rounded'></input>
+                <input type='password' placeholder='Password' className='p-2 border border-gray-300 rounded'></input>
+                <button type='submit' className='bg-orange-500 text-white font-bold p-2 rounded hover:bg-orange-600'>Submit</button>
+                <button type='account' className='bg-gray-400 text-white font-bold p-2 rounded hover:bg-gray-500'>Create New Account</button>
+            </form>
         </div>
     )
 }
